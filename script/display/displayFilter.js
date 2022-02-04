443de4256a668b6f2110dc1c35d30699af1ca45f
laurent972 <laurent.digitick@gmail.com>
2022-02-04 10:23:40 +0100

    correction bug - ajout multiples criteres non voulu

--- script/display/displayFilter.js
+++ script/display/displayFilter.js
@@ -1,19 +1,13 @@
 
 // Filtre ingredients
- async function displayIngredients(searchTerm){
-
-  if(searchTerm == undefined){
-    searchTerm = ''
-  }
+ async function displayIngredients(){
   recettes = await fetchSearch();
- 
     recettes
-    .filter(recette => recette.ingredients.map(ingredient => ingredient.ingredient).toString().toLowerCase().includes(searchTerm))
+    .filter(recette => recette.ingredients.map(ingredient => ingredient.ingredient).toString().toLowerCase())
     .map((recette) => {
      recette.ingredients.map(ingredient =>  ingredientsList = ingredientsList.concat(ingredient.ingredient.toLowerCase()))
      ingredientsList = [...new Set(ingredientsList)]
      ingredients.innerHTML = 
-       
       ingredientsList.map(ingredient =>`<li><a class="dropdown-item" href="#">${ingredient}</a></li>`).join('')
       return ingredientsList
     })
@@ -23,19 +17,16 @@ displayIngredients();
 
 
 //filtre Appareils
- async function displayAppliances(searchTerm){
-  if(searchTerm == undefined){
-    searchTerm = ''
-  }
+ async function displayAppliances(){
+
   recettes = await fetchSearch();
     recettes
-    .filter(recette => recette.appliance.toLowerCase().includes(searchTerm))
+    .filter(recette => recette.appliance.toLowerCase())
     .map(recette => {
       applianceList.push(recette.appliance.toLowerCase());
       applianceList = [...new Set(applianceList)];
       appareils.innerHTML =
       applianceList.map(appliance => `<li><a class="dropdown-item" href="#">${appliance}</a><li>`).join('');
-
     })
     selectAppliance();
   }
@@ -43,19 +34,15 @@ displayIngredients();
 
 
 //filtre Appareils
- async function displayUstensils(searchTerm){
-  if(searchTerm == undefined){
-    searchTerm = ''
-  }
+ async function displayUstensils(){
   recettes = await fetchSearch();
     recettes
-    .filter(recette => recette.ustensils.map(ustensil => ustensil).toString().toLowerCase().includes(searchTerm))
+    .filter(recette => recette.ustensils.map(ustensil => ustensil).toString().toLowerCase())
     .map(recette => {
       recette.ustensils.map(ustensil =>  ustensilList = ustensilList.concat(ustensil.toLowerCase()))
       ustensilList = [...new Set(ustensilList)];
       ustensils.innerHTML =
       ustensilList.map(ustensil => `<li><a class="dropdown-item" href="#">${ustensil}</a><li>`).join('');
-
     })
     selectUstensil()
   }
